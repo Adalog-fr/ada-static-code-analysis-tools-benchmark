c6acb8555e9b7e08065f6cef512bc4cce8c744aa
Heziode <git.life@titamail.com>
2023-06-28 00:28:49 +0200

    fix(generate-env): generate the right `.env` in the right place

--- utils/modules/generate_env.ts
+++ utils/modules/generate_env.ts
@@ -1,14 +1,28 @@
 import { Command } from "https://deno.land/x/cmd@v1.2.0/mod.ts";
 import ProgressBar from "https://deno.land/x/progress@v1.3.8/mod.ts";
 import { initializeModule as initializeCommandModule } from "./runCommandFactory.ts";
-import { join } from "https://deno.land/std/path/mod.ts";
+
 export function initializeModule(program: Command): void {
     let progress : ProgressBar;
     let completed = 0;
     initializeCommandModule(program, {
         description: "Generate a '.env' file for every `alire.toml` files fond in `alireTomlPath`.",
         commandName: "generate-env",
-        command: ["alr", ["printenv"]],
+        command: [
+            "deno",
+            [
+                "run",
+                "--config",
+                "/workspaces/bench-source/deno.jsonc",
+                "--allow-read",
+                "--allow-write",
+                "--allow-env",
+                "--allow-run",
+                "--allow-ffi",
+                "--unstable",
+                "/workspaces/bench-source/utils/executeEnvFileGeneration.ts",
+            ]
+        ],
         concurrency: 8,
         beforeRun: (params) => {
             const title = "generating:";
@@ -22,24 +36,10 @@ export function initializeModule(program: Command): void {
             progress.render(completed++);
 
             if (!task.result?.success) {
+                console.error(task.result.output);
+
                 return;
             }
-
-            const lines = task.result.output.split("\n");
-
-            const envVariables: string[] = [];
-            for (const line of lines) {
-                const match = line.match(/export\s+([A-Z_]+)=(.+)/);
-                if (match) {
-                    const key = match[1];
-                    const value = match[2];
-                    envVariables.push(`${key}=${value}`);
-                }
-            }
-
-            const envContent = envVariables.join("\n");
-
-            Deno.writeTextFileSync(join(task.task.data.path, ".env"), envContent);
         }
     });
 }
