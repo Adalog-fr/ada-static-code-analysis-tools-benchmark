694dd9733a018cea9b73cd16c6ea256b5b5904e8
Heziode <git.life@titamail.com>
2024-07-10 00:37:36 +0200

    refactor: Simplify benchmark script and enhance output
    The benchmark script has been streamlined and improved:
    
    - Removed the clean_logs function and its calls, as it is no needed
    - Simplified the run_benchmark function to take fewer arguments
    - Adjusted the loop syntax to work
    - Enhanced the console output to provide clearer iteration progress and labeling
    - Commented out the actual benchmark runs for now
    
    These changes make the script more concise, easier to understand, and improve the overall developer experience when running and monitoring benchmark iterations.

--- benchmark.sh
+++ benchmark.sh
@@ -14,30 +14,25 @@ function run_benchmark() {
     local script_name="$1"
     local xp_num="$2"
     local j_option="$3"
-    local iteration="$4"
     local log_file="/workspaces/bench-source/${script_name}-${xp_num}-${j_option}.log"
 
     # Run the benchmark script with time and redirect output to the log file
-    time ./${script_name} -xpNum ${xp_num} ${j_option} 2>> ${log_file}
-}
-
-# Function to clean up logs using a Deno script
-function clean_logs() {
-    deno run --allow-all utils/deleteBenchmarkLogs.ts
+    time ./${script_name} -xpNum ${xp_num} -j ${j_option} 2>> ${log_file}
 }
 
 # Main loop to run benchmarks for Adactl_benchmark.sh
-for i in {1..$maxIteration}; do
+for i in $(seq 1 $maxIteration); do
     echo "Running Adactl_benchmark.sh iteration $i/$maxIteration"
-    run_benchmark "Adactl_benchmark.sh" "$i" "-j0" "$i"
-    clean_logs
+    #run_benchmark "Adactl_benchmark.sh" "$i" "1"
 done
 
+echo ""
+
 # Main loops to run benchmarks for GNATcheck_benchmark.sh with -j1 and -j32
-for j_option in "-j1" "-j32"; do
-    for i in {1..$maxIteration}; do
-        echo "Running GNATcheck_benchmark.sh iteration $i/$maxIteration with $j_option"
-        run_benchmark "GNATcheck_benchmark.sh" "$i" "$j_option" "$i"
-        clean_logs
+for j_option in "1" "32"; do
+    for i in $(seq 1 $maxIteration); do
+        echo "Running GNATcheck_benchmark.sh iteration $i/$maxIteration with $j_option thread(s)"
+        #run_benchmark "GNATcheck_benchmark.sh" "$i" "$j_option"
     done
+    echo ""
 done
