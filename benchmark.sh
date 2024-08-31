2c4dc175e732ee11e5a2c6421dbe800d786d76a4
Heziode <git.life@titamail.com>
2024-08-31 10:21:35 +0200

    feat: enhance benchmark script with improved options and cogralys support
    - Add command line options for max iterations, Neo4j connection details
    - Support running cogralys benchmarks in addition to AdaControl and GNATcheck
    - Add help option to display usage information

--- benchmark.sh
+++ benchmark.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-# Currently, this script run 10 times the following configurations:
+# By default, this script run 10 times the following configurations:
 #  - AdaControl
 #  - GNATcheck, monothread
 #  - GNATcheck, 32 threads
-#
-# TODO: add cogralyis
+#  - cogralys
 
 maxIteration=10
+PROJECT_ROOT=$PWD
 
-# Function to run a benchmark script with the given parameters
-function run_benchmark() {
-    local script_name="$1"
-    local xp_num="$2"
-    local j_option="$3"
-    local log_file="/workspaces/bench-source/${script_name}-${xp_num}-${j_option}.log"
-
-    # Run the benchmark script with time and redirect output to the log file
-    time ./${script_name} -xpNum ${xp_num} -j ${j_option} 2>> ${log_file}
+# Function to display help information
+show_help() {
+    echo "Usage: $0 [--maxIteration <number>] [-h|--help]"
+    echo "  --maxIteration <number>   How many times the script should run the benchmark."
+    echo "  -n, --neo4jHost <URI>     Bolt URI of Neo4j database, with port (e.g.: bolt://domain.com:7687)."
+    echo "  --username <userName>     Username used to login to Neo4j database."
+    echo "  --password <password>     Password used to login to Neo4j database."
+    echo "  -h, --help                Show help information."
 }
+
+# Parse command line arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        --maxIteration) maxIteration="$2"; shift 2 ;;
+        -h|--help) show_help; exit 0 ;;
+        -n|--neo4jHost) NEO4J_HOST="$2"; shift 2 ;;
+        --username) NEO4J_USER="$2"; shift 2 ;;
+        --password) NEO4J_PASS="$2"; shift 2 ;;
+        *) echo "Unknown option: $1"; show_help; exit 1 ;;
+    esac
+done
+
+# BENCHMARK #
 
 # Main loop to run benchmarks for Adactl_benchmark.sh
 for i in $(seq 1 $maxIteration); do
     echo "Running Adactl_benchmark.sh iteration $i/$maxIteration"
-    run_benchmark "Adactl_benchmark.sh" "$i" "1"
+    ./Adactl_benchmark.sh -xpNum "$i"
 done
 
 echo ""
@@ -32,7 +45,15 @@ echo ""
 for j_option in "1" "32"; do
     for i in $(seq 1 $maxIteration); do
         echo "Running GNATcheck_benchmark.sh iteration $i/$maxIteration with $j_option thread(s)"
-        run_benchmark "GNATcheck_benchmark.sh" "$i" "$j_option"
+        ./GNATcheck_benchmark.sh -xpNum "$i" -j "$j_option"
     done
     echo ""
+done
+
+echo ""
+
+# Main loop to run benchmarks for cogralys_benchmark.sh
+for i in $(seq 1 $maxIteration); do
+    echo "Running cogralys_benchmark.sh iteration $i/$maxIteration"
+    ./cogralys_benchmark.sh -xpNum "$i" --neo4jHost "$neo4jHost" --username $username --password $password
 done
